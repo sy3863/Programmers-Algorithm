f8444f0ca2a49d1b7d2d9d3fe10e53ada2fa0a52
soyun <92129511+sy3863@users.noreply.github.com>
2024-01-28 17:23:52 +0900

    Create 콜라 문제.java
    프로그래머스 Lv1 콜라 문제

---
+++ Lv1/콜라 문제.java
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    int remain = 0;
+    while(n >= a){
+        int result = ((n) / a) * b;
+        remain = ((n) % a);
+        answer += result;
+        n = result + remain;
+    }
+    return answer;
+}
